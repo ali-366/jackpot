2c0b523dde486d961a58677e5346ecfdfb81141c
HOSSIAN MOHAMMOD ALI <you@example.com>
2021-12-12 23:54:00 +0900

    90% ready

---
+++ js/custom.js
@@ -0,0 +1,57 @@
+
+var faq = document.getElementsByClassName("faq-page");
+var i;
+
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active2");
+
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
+
+
+
+// get elements froom html
+var hours = document.getElementById('hours'); 
+var minutes = document.getElementById('minutes'); 
+var seconds = document.getElementById('seconds'); 
+
+var currentYear = new Date().getFullYear();
+
+var nextYear = new Date(`may 08 ${currentYear + 1} 00:00:00`);
+
+
+function countdown()
+{
+    var currentTime = new Date();
+    var diff = nextYear - currentTime;
+    var s = Math.floor(diff/1000) % 60;
+    var m = Math.floor(diff/1000/60) % 60; 
+    var h = Math.floor(diff/1000/60/60) % 24;
+
+    seconds.innerHTML = s;
+    minutes.innerHTML = m;
+    hours.innerHTML = h;
+}
+setInterval(countdown,1000);
+//  plyer slick
+    // testimonail slider 
+  $(document).ready(function(){
+      $('.click_player').slick({
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        slidesToShow: 2, 
+        vertical:true,
+        centerMode: true,
+      });
+  });
